b741ab0a51d48ed7b943822714aee607e0a00602
tomatteo <matteothome1@gmail.com>
2026-05-22 17:24:40 -0300

    fazendo o livro.h e livro.c

--- livro.c
+++ livro.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+#include "livro.h"
 
-struct Livro {
-    int id;
-    char titulo[50];
-    char autor[50];
-    int ano_publicacao;
-    int quantidade;
-};
+struct Livro criar_livro() {
+    struct Livro l;
+
+    printf("Digite o ID: ");
+    scanf("%d", &l.id);
+    getchar(); 
+
+    printf("Digite o titulo: ");
+    fgets(l.titulo, 50, stdin);
+    l.titulo[strcspn(l.titulo, "\n")] = '\0'; 
+
+    printf("Digite o autor: ");
+    fgets(l.autor, 50, stdin);
+    l.autor[strcspn(l.autor, "\n")] = '\0'; 
+
+    printf("Digite o ano: ");
+    scanf("%d", &l.ano_publicacao);
+
+    printf("Digite a quantidade: ");
+    scanf("%d", &l.quantidade);
+
+    return l;
+}
+
+void exibir_livro(struct Livro l) {
+    printf("--- Detalhes do Livro ---\n");
+    printf("ID: %d\n", l.id);
+    printf("Titulo: %s\n", l.titulo);
+    printf("Autor: %s\n", l.autor);
+    printf("Ano: %d\n", l.ano_publicacao);
+    printf("Quantidade em estoque: %d\n", l.quantidade);
+}
---
+++ livro.h
@@ -0,0 +1,15 @@
+#ifndef LIVRO_H
+#define LIVRO_H
+
+struct Livro {
+    int id;               
+    char titulo[50];
+    char autor[50];
+    int ano_publicacao;
+    int quantidade;
+};
+
+struct Livro criar_livro();
+void exibir_livro(struct Livro l);
+
+#endif
