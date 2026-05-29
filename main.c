6029ec10c4b3c4d2615792e8d408feebb8986232
JPLoso <pedrovelosoj5@gmail.com>
2026-05-29 19:57:42 -0300

    main.c

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "livro.h"
 #include "lista.h"
 
 int main() {
-
     No* biblioteca = criar_lista();
-
     int opcao;
 
     do {
-
         printf("\n===== SISTEMA BIBLIOTECA =====\n");
         printf("1 - Cadastrar livro\n");
         printf("2 - Exibir biblioteca\n");
@@ -21,40 +17,28 @@ int main() {
         getchar();
 
         switch(opcao) {
-
             case 1: {
-
                 struct Livro novoLivro;
-
                 novoLivro = criar_livro();
-
                 inserir_livro(&biblioteca, novoLivro);
-
                 printf("\nLivro cadastrado com sucesso!\n");
-
                 break;
             }
 
             case 2: {
-
                 printf("\n===== LIVROS CADASTRADOS =====\n");
-
                 exibir_biblioteca(biblioteca);
-
                 break;
             }
 
             case 0: {
-
                 printf("\nEncerrando sistema...\n");
-
                 break;
             }
-
+            
             default:
                 printf("\nOpcao invalida!\n");
         }
-
     } while(opcao != 0);
 
     return 0;
